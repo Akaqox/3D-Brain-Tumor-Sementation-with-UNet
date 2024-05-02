60eae8a8f0fd384f940836de3fe69cc335a8b8c7
solauphoenix <96625984+solauphoenix@users.noreply.github.com>
2024-05-02 18:48:20 +0300

    environment tries
    Trying to read data and testing the libraries

--- src/data_read.py
+++ src/data_read.py
@@ -6,3 +6,59 @@ Created on Wed May  1 19:23:17 2024
 @author: solauphoenix
 """
 
+
+
+import os
+import cv2
+import glob
+import PIL
+import shutil
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from skimage import data
+from skimage.util import montage 
+import skimage.transform as skTrans
+from skimage.transform import rotate
+from skimage.transform import resize
+from PIL import Image, ImageOps  
+
+
+# neural imaging
+import nilearn as nl
+import nibabel as nib
+import nilearn.plotting as nlplt
+import gif_your_nifti.core as gif2nif
+
+
+# ml libs
+import keras
+import keras.backend as K
+from keras.callbacks import CSVLogger
+import tensorflow as tf
+from tensorflow.keras.utils import plot_model
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from tensorflow.keras.models import *
+from tensorflow.keras.layers import *
+from tensorflow.keras.optimizers import *
+from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping, TensorBoard
+from tensorflow.keras.layers.experimental import preprocessing
+
+
+
+data_path = "../archive/BraTS2021_00621/"
+path = os.path.join(data_path, 'BraTS2021_00621_flair.nii.gz')
+
+img  = nib.load(path)
+nii_data = img.get_fdata()
+nii_aff  = img.affine
+nii_hdr  = img.header
+print(nii_aff ,'\n',nii_hdr)
+print(nii_data.shape)
+
+
+display = nl.plotting.plot_img(img)
+nl.plotting.show()
